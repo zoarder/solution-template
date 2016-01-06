e29c3a6939ab2fe6ff2fce0a5a67c1d4d8c9480d
Faisal Ahmed <faisal.ahmed@tigeritbd.com>
2016-01-06 20:16:31 +0600

    fixed build error

--- src/main/java/com/tigerit/solution/Solution.java
+++ src/main/java/com/tigerit/solution/Solution.java
@@ -6,7 +6,6 @@ public class Solution {
     public static void main(String... args) {
         System.out.println("running solution ...");
         InputOutput io = new InputOutput();
-        System.out.println(io.read());
-        System.out.println(io.write());
+        io.read(); io.write();
     }
 }
