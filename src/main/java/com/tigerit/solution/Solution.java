2c67b3ceb77fb96824d04b7508fec50371aedc3e
Faisal Ahmed <faisal.ahmed@tigeritbd.com>
2016-01-06 20:11:31 +0600

    imporved POC

--- src/main/java/com/tigerit/solution/Solution.java
+++ src/main/java/com/tigerit/solution/Solution.java
@@ -1,7 +1,12 @@
 package com.tigerit.solution;
 
+import com.tigerit.io
+
 public class Solution {
     public static void main(String... args) {
         System.out.println("running solution ...");
+        InputOutput io = new InputOutput();
+        System.out.println(io.read());
+        System.out.println(io.write());
     }
 }
