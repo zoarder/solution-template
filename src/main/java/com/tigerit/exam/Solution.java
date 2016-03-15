ba164b4d69bad88e040d07ca1364f96d9a62faae
Faisal Ahmed <faisal.ahmed@tigeritbd.com>
2016-03-15 16:06:07 +0600

    updated template

--- src/main/java/com/tigerit/exam/Solution.java
+++ src/main/java/com/tigerit/exam/Solution.java
@@ -1,6 +1,11 @@
 package com.tigerit.exam;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
  * All of your application logic should be placed inside this class.
  * Remember we will load your application from our custom container.
@@ -11,7 +16,11 @@ public class Solution implements Runnable {
     @Override
     public void run() {
         // your application logic goes here.
-        String name = System.console().readLine();
-        System.out.println("Hello " + name);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("Hello " + reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
